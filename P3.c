5f0fb534ca1556d1eb40f578ba35fd28703dab10
Sindhu R <sindhur24cs@rnsit.ac.in>
2026-03-17 15:02:18 +0530

    Insertion Sort

---
+++ P3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {9, 3, 5, 1, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift elements greater than key
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
